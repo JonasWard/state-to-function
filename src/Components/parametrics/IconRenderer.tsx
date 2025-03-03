d98bf935fc2fe05c1198f335371277882dbb9be8
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-03 16:56:29 +0100

    feat: fixed and simplified styling for subscript

--- src/Components/parametrics/IconRenderer.tsx
+++ src/Components/parametrics/IconRenderer.tsx
@@ -28,21 +28,9 @@ export const getIconForKey = (name: string, type?: DataType) => {
 export const IconRenderer: React.FC<IconRendererProps> = ({ name, type, size = 20 }) => {
   const { mainIcon, subscript } = getIconForKey(name, type);
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', height: size * 1.3 }}>
-      <div style={{ fontSize: size, alignItems: 'center' }}>{mainIcon}</div>
-      {subscript ? (
-        <div
-          style={{
-            fontSize: size * 0.5,
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          {subscript ? <div key='subscript'>{subscript}</div> : <div style={{ height: '50%' }} />}
-        </div>
-      ) : (
-        <span style={{ marginLeft: 10 }}>{name}</span>
-      )}
-    </div>
+    <span>
+      <var style={{ fontSize: size, alignItems: 'center' }}>{mainIcon}</var>
+      {subscript && <sub>{subscript}</sub>}
+    </span>
   );
 };
