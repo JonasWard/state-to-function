6e3ee14c06f6ec9bd855780d15414d052d72d744
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-03 16:51:42 +0100

    feat: accepted characters

---
+++ src/modelDefinition/enums/chars.ts
@@ -0,0 +1,3 @@
+export const validScientificSymbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZανβξΓγΔδΠπερζΣσςητΘΦφχλψμΩω0123456789?';
+export const validScientificDescriptors = 'abcdefghijklmnopqrstuvwxyz0123456789-_.,αβγεφμ+-()[]{}$*#~/<>!? ';
+export const validDescriptors = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 0123456789.';
