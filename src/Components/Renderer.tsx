7a24a3d8122effabed3e97d324fa9bd24fcbf670
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-03 19:45:31 +0100

    feat: simple json-style renderer of data model

---
+++ src/Components/Renderer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useData } from '../state/state';
+import { DataEntry, DerivativeStateDataType, StateDataType } from 'url-safe-bitpacking';
+
+const isDataEntry = (data: StateDataType | DataEntry | DerivativeStateDataType) =>
+  Object.hasOwn(data as DataEntry, 'type') && Object.hasOwn(data as DataEntry, 'value');
+
+const GeneralObjectRenderer: React.FC<{ data: StateDataType | DataEntry | DerivativeStateDataType }> = ({ data }) => {
+  if (isDataEntry(data)) return `${(data as DataEntry).type} ${(data as DataEntry).name}`;
+  else if (Object.hasOwn(data as DerivativeStateDataType, 'v') && Object.hasOwn(data as DerivativeStateDataType, 's'))
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: 8 }}>
+        <span>
+          <GeneralObjectRenderer data={(data as DerivativeStateDataType).s} />
+        </span>
+        {Array.isArray((data as DerivativeStateDataType).v) ? (
+          ((data as DerivativeStateDataType).v as StateDataType[]).map((nestedData) => <GeneralObjectRenderer data={nestedData} />)
+        ) : (
+          <GeneralObjectRenderer data={(data as DerivativeStateDataType).v as StateDataType} />
+        )}
+      </div>
+    );
+  else
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        {Object.entries(data as StateDataType).map(([aName, nestedData]) => (
+          <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: 8 }}>
+            <span>{aName}</span>
+            <GeneralObjectRenderer data={nestedData} />
+          </div>
+        ))}
+      </div>
+    );
+};
+
+export const Renderer: React.FC = () => {
+  const data = useData((s) => s.data);
+
+  // return JSON.stringify(getStateValue(data), null, '\t');
+
+  return <GeneralObjectRenderer data={data} />;
+};
