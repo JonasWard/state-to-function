b5e0ec7df763cf9707e9166d4d82f58c7ba91566
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-08 10:43:46 +0100

    chore: migrated object renderer

--- src/Components/renderers/StateDataRenderer.tsx
+++ src/Components/renderers/StateDataRenderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useData } from '../state/state';
+import { useData } from '../../state/state';
 import { DataEntry, DerivativeStateDataType, StateDataType } from 'url-safe-bitpacking';
+import { MethodRenderer } from './MethodRenderer';
 
 const isDataEntry = (data: StateDataType | DataEntry | DerivativeStateDataType) =>
   Object.hasOwn(data as DataEntry, 'type') && Object.hasOwn(data as DataEntry, 'value');
@@ -33,10 +34,10 @@ const GeneralObjectRenderer: React.FC<{ data: StateDataType | DataEntry | Deriva
     );
 };
 
-export const Renderer: React.FC = () => {
+export const StateDataRenderer: React.FC = () => {
   const data = useData((s) => s.data);
 
   // return JSON.stringify(getStateValue(data), null, '\t');
-
+  return <MethodRenderer />;
   return <GeneralObjectRenderer data={data} />;
 };
