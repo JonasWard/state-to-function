dd66e792e65eff82474c0a82241e3cf50f276e4e
Jonas Ward <jonas.vandenbulcke@gmail.com>
2026-01-12 22:15:36 +0100

    chore: improve styling of .method-flat hover

--- src/Components/renderers/method/MethodOperation.tsx
+++ src/Components/renderers/method/MethodOperation.tsx
@@ -1,13 +1,12 @@
-import { Button } from 'antd';
 import React from 'react';
 import { MethodHandlingProps, ShortSymbol } from './methodType';
 
 export const MethodOperation: React.FC<MethodHandlingProps> = (props) => (
-  <Button type="text" size="small" style={{ padding: 1 }}>
+  <span className="method-operation">
     {props.node.descriptor.mapping[props.node.state] === 'division' ? (
       <span />
     ) : (
       ShortSymbol[props.node.descriptor.mapping[props.node.state]]
     )}
-  </Button>
+  </span>
 );
